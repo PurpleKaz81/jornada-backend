375a6b80706765cba46b55152a18618e7387c342
rafael kasinski <rakasin@gmail.com>
2023-07-07 18:38:26 -0300

    Add Testimonials model with photo, text, and name fields

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Testimonials(models.Model):
+    photo = models.ImageField(upload_to='testimonials')
+    text = models.TextField()
+    name = models.CharField(max_length=255)
