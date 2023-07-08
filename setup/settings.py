8156dbf5ba77b37740de8e6d3843066c538519f5
rafael kasinski <rakasin@gmail.com>
2023-07-07 22:46:03 -0300

    Add images to photos folder and update settings.py with allowed hosts and rest_framework

--- setup/settings.py
+++ setup/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv("JORNADA_SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ""]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "testimonials",
     "imagekit",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
